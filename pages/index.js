b34810ad26fbf25964fea7c3088cd8f5a49f9ddc
BlockchainAcademyBD <103687652+BlockchainAcademyBD@users.noreply.github.com>
2022-07-31 01:59:09 +0600

    gtag added

--- pages/index.js
+++ pages/index.js
@@ -224,3 +224,13 @@ export default withTheme(Home)
 //   }
 //
 // }
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-13ZQWBGB62"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-13ZQWBGB62');
+</script>
